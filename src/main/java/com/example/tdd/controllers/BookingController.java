1f5d7f53fa39b692dbd91bb6fdb0279962de6a62
FoxGreedy <hotmus8500@gmail.com>
2021-05-26 17:32:35 -0300

    feat(booking): create save route and test

--- src/main/java/com/example/tdd/controllers/BookingController.java
+++ src/main/java/com/example/tdd/controllers/BookingController.java
@@ -1,9 +1,15 @@
 package com.example.tdd.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.tdd.models.BookingModel;
 
 @RestController
 @RequestMapping("/bookings")
@@ -13,6 +19,11 @@ public class BookingController {
 	@ResponseBody
 	public String getAll() {
 		return "Ok";
+	}
+
+	@PostMapping
+	public ResponseEntity<BookingModel> save(@RequestBody BookingModel bookingModel) {
+		return ResponseEntity.status(HttpStatus.OK).body(bookingModel);
 	}
 
 }
